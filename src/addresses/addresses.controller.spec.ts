dfb662cbb94dbe5146983c10ce5ef6d603fa3ddd
Karen Gasparyan <karen@firstescrow.co.uk>
2022-12-20 11:39:29 +0400

    added integration test

--- src/addresses/addresses.controller.spec.ts
+++ src/addresses/addresses.controller.spec.ts
@@ -1,8 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AddressesController } from './addresses.controller';
+import {Response} from "express";
+import {RequestType} from "../dto/main.dto";
 
 describe('AddressesController', () => {
   let controller: AddressesController;
+
+  const requestMock = {
+    body: {}
+  } as unknown as RequestType;
+
+  const statusResponseMock = {
+    send: jest.fn(x => x)
+  }
+
+  const responseMock = {
+    statusCode: jest.fn(x => statusResponseMock),
+    send: jest.fn(x => x)
+  } as unknown as Response;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,4 +30,16 @@ describe('AddressesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('AddressesFunctions',() => {
+    it('should return a status 422', () => {
+      controller.create(requestMock, {
+        address: '',
+        city: '',
+        country: '',
+        zip: ''
+      });
+      expect(responseMock.statusCode).toHaveBeenCalledWith(422);
+    });
+  })
 });

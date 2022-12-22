6d08638b2c0107aee1ec556c703f95b2b116791d
Eduard <eduardmelkonyan.evistep@gmail.com>
2022-12-22 17:34:33 +0400

    Added tryCatches

--- src/educations/educations.controller.ts
+++ src/educations/educations.controller.ts
@@ -1,10 +1,30 @@
-import {Body, Controller, Delete, Get, Param, Post, Put, Req} from '@nestjs/common';
-import {ApiBody, ApiHeader, ApiParam, ApiResponse, ApiTags} from "@nestjs/swagger";
-import {EducationsService} from "./educations.service";
-import {xAuthorization} from "../swagger/main";
-import {RequestType, uuId} from "../dto/main.dto";
-import {createEducationBody, createEducationResponse, getEducationResponse} from "../swagger/educations";
-import {educations} from "../dto/educations.dto";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+} from '@nestjs/common';
+import {
+  ApiBody,
+  ApiHeader,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { EducationsService } from './educations.service';
+import { xAuthorization } from '../swagger/main';
+import { RequestType, uuId } from '../dto/main.dto';
+import {
+  createEducationBody,
+  createEducationResponse,
+  getEducationResponse,
+} from '../swagger/educations';
+import { educations } from '../dto/educations.dto';
+import { catchError } from '../utils/helpers';
 
 @ApiTags('Educations')
 @Controller('education')
@@ -15,26 +35,34 @@ export class EducationsController {
   @ApiHeader(xAuthorization)
   @ApiResponse(createEducationResponse)
   @ApiBody(createEducationBody)
-  async create(@Req() req: RequestType, @Body() body: educations){
-    const {id} = req.user;
-    const data = await this.educationsService.create(id, body);
-    return {
-      statusCode: 201,
-      message: 'Education has been created successfully',
-      data
+  async create(@Req() req: RequestType, @Body() body: educations) {
+    try {
+      const { id } = req.user;
+      const data = await this.educationsService.create(id, body);
+      return {
+        statusCode: 201,
+        message: 'Education has been created successfully',
+        data,
+      };
+    } catch (e) {
+      return catchError(e);
     }
   }
 
   @Get('/')
   @ApiHeader(xAuthorization)
   @ApiResponse(getEducationResponse)
-  async getList(@Req() req: RequestType){
-    const {id} = req.user;
-    const data = await this.educationsService.getList(id);
-    return {
-      statusCode: 200,
-      message: 'Educations list',
-      data
+  async getList(@Req() req: RequestType) {
+    try {
+      const { id } = req.user;
+      const data = await this.educationsService.getList(id);
+      return {
+        statusCode: 200,
+        message: 'Educations list',
+        data,
+      };
+    } catch (e) {
+      return catchError(e);
     }
   }
 
@@ -43,16 +71,20 @@ export class EducationsController {
   @ApiResponse(createEducationResponse)
   @ApiParam({
     name: 'id',
-    type: 'string'
+    type: 'string',
   })
-  async getById(@Req() req: RequestType, @Param() param: uuId){
-    const {id: user_id} = req.user;
-    const {id} = param;
-    const data = await this.educationsService.getById(user_id, id);
-    return {
-      statusCode: 200,
-      message: 'Get Education',
-      data
+  async getById(@Req() req: RequestType, @Param() param: uuId) {
+    try {
+      const { id: user_id } = req.user;
+      const { id } = param;
+      const data = await this.educationsService.getById(user_id, id);
+      return {
+        statusCode: 200,
+        message: 'Get Education',
+        data,
+      };
+    } catch (e) {
+      return catchError(e);
     }
   }
   @Put('/:id')
@@ -61,16 +93,24 @@ export class EducationsController {
   @ApiBody(createEducationBody)
   @ApiParam({
     name: 'id',
-    type: 'string'
+    type: 'string',
   })
-  async update(@Req() req: RequestType, @Param() param: uuId, @Body() body: educations){
-    const {id: user_id} = req.user;
-    const {id} = param;
-    const data = await this.educationsService.update(user_id, id, body);
-    return {
-      statusCode: 200,
-      message: 'Education has been updated successfully',
-      data
+  async update(
+    @Req() req: RequestType,
+    @Param() param: uuId,
+    @Body() body: educations,
+  ) {
+    try {
+      const { id: user_id } = req.user;
+      const { id } = param;
+      const data = await this.educationsService.update(user_id, id, body);
+      return {
+        statusCode: 200,
+        message: 'Education has been updated successfully',
+        data,
+      };
+    } catch (e) {
+      return catchError(e);
     }
   }
 
@@ -79,16 +119,20 @@ export class EducationsController {
   @ApiResponse(createEducationResponse)
   @ApiParam({
     name: 'id',
-    type: 'string'
+    type: 'string',
   })
-  async destroy(@Req() req: RequestType, @Param() param: uuId){
-    const {id: user_id} = req.user;
-    const {id} = param;
-    const data = await this.educationsService.destroy(user_id, id);
-    return {
-      status: 200,
-      message: 'Education has been removed successfully',
-      data
+  async destroy(@Req() req: RequestType, @Param() param: uuId) {
+    try {
+      const { id: user_id } = req.user;
+      const { id } = param;
+      const data = await this.educationsService.destroy(user_id, id);
+      return {
+        status: 200,
+        message: 'Education has been removed successfully',
+        data,
+      };
+    } catch (e) {
+      return catchError(e);
     }
   }
 }

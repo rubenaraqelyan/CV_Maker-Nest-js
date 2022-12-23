6c2b543b406e0eba7ac8eee964fc7dbd58a0a58a
Eduard <eduardmelkonyan.evistep@gmail.com>
2022-12-23 18:30:51 +0400

    updated

--- src/utils/HttpError.ts
+++ src/utils/HttpError.ts
@@ -2,14 +2,14 @@ export default class HttpError extends Error {
   statusCode: number;
   messages: object;
 
-  constructor(props: { statusCode: number, message: string, messages?: object }) {
+  constructor(props: { statusCode: number, messageType: string, messages?: object }) {
     super()
-    const {statusCode, message, messages} = props;
+    const {statusCode, messageType, messages} = props;
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, HttpError)
     }
     this.statusCode = statusCode;
-    this.message = message;
+    this.message = messageType;
     this.messages = messages
     this.name = 'Http error';
   }

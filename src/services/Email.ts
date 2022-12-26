0d985778f8a1f6a5c98d5ccbb0c405dbb41bbaf4
Syuzi <syuzi.evistep@gmail.com>
2022-12-26 18:08:55 +0400

    fixied mail issue

--- src/services/Email.ts
+++ src/services/Email.ts
@@ -5,7 +5,7 @@ const {MAIL_FROM, MAIL_HOST, MAIL_PORT, MAIL_USER, MAIL_PASSWORD} = process.env;
 const transporter = nodemailer.createTransport({
   host: MAIL_HOST,
   port: MAIL_PORT,
-  secure: true,
+  // secure: true,
   auth: {
     user: MAIL_USER,
     pass: MAIL_PASSWORD,
